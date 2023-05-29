OpenEmojiBattler/open-emoji-battler

commit a07a8de7add5f541bd32d35f7fe7bdaeb5fe3be9

Fix front accordingly for ink4

front/src/components/App/ConnectionProvider/Contract/tasks.ts
@@ -7,18 +7,15 @@ import { buildEmoBases } from "~/misc/mtcUtils"
 import type { Connection } from "../tasks"
 
 export const buildConnection = async (api: ApiPromise, env: EnvContract): Promise<Connection> => {
-  const gameContract = getGameContract(api, env.gameAddress)
+  const gameContract = getGameContract(api, env.gameAddress, env.ink)
 
-  const codec = createType(
-    "Option<emo_Bases>",
-    (await queryContract(gameContract, "getEmoBases")).toU8a()
-  ).unwrap()
-
-  const emoBases = buildEmoBases(codec)
+  const emoBases = buildEmoBases(
+    (await query(gameContract, env.ink, "Option<emo_Bases>", "getEmoBases")).unwrap()
+  )
 
   return {
     kind: "contract",
-    query: buildConnectionQuery(gameContract),
+    query: buildConnectionQuery(gameContract, env.ink),
     tx: buildConnectionTx(gameContract),
     emoBases,
     api: () => api,
@@ -26,52 +23,62 @@ export const buildConnection = async (api: ApiPromise, env: EnvContract): Promis
   }
 }
 
-const buildConnectionQuery = (gameContract: ContractPromise): Connection["query"] => ({
+const query = async (
+  contract: ContractPromise,
+  inkVersion: number,
+  type: string,
+  fnName: string,
+  fnArgs: any[] = []
+) => {
+  const response = (await queryContract(contract, fnName, fnArgs)).toU8a()
+
+  switch (inkVersion) {
+    case 3:
+      return createType(type, response)
+    case 4:
+      return (createType(`Result<${type}, ()>`, response) as any).asOk
+    default:
+      throw new Error(`undefined ink version: ${inkVersion}`)
+  }
+}
+
+const buildConnectionQuery = (
+  gameContract: ContractPromise,
+  inkVersion: number
+): Connection["query"] => ({
   deckFixedEmoBaseIds: async () =>
-    createType(
-      "Option<Vec<u16>>",
-      (await queryContract(gameContract, "getDeckFixedEmoBaseIds")).toU8a()
-    ).unwrap(),
+    (await query(gameContract, inkVersion, "Option<Vec<u16>>", "getDeckFixedEmoBaseIds")).unwrap(),
   deckBuiltEmoBaseIds: async () =>
-    createType(
-      "Option<Vec<u16>>",
-      (await queryContract(gameContract, "getDeckBuiltEmoBaseIds")).toU8a()
-    ).unwrap(),
-  matchmakingGhostsInfo: async (band) =>
-    createType(
+    (await query(gameContract, inkVersion, "Option<Vec<u16>>", "getDeckBuiltEmoBaseIds")).unwrap(),
+  matchmakingGhostsInfo: (band) =>
+    query(
+      gameContract,
+      inkVersion,
       "Option<Vec<(BlockNumber, AccountId)>>",
-      (await queryContract(gameContract, "getMatchmakingGhostsInfo", [band])).toU8a()
+      "getMatchmakingGhostsInfo",
+      [band]
     ),
-  matchmakingGhostByIndex: async (band, index) =>
-    createType(
-      "Option<mtc_Ghost>",
-      (await queryContract(gameContract, "getMatchmakingGhostByIndex", [band, index])).toU8a()
+  matchmakingGhostByIndex: (band, index) =>
+    query(gameContract, inkVersion, "Option<mtc_Ghost>", "getMatchmakingGhostByIndex", [
+      band,
+      index,
+    ]),
+  leaderboard: () => query(gameContract, inkVersion, "Vec<(u16, AccountId)>", "getLeaderboard"),
+  playerEp: (address) => query(gameContract, inkVersion, "Option<u16>", "getPlayerEp", [address]),
+  playerSeed: (address) =>
+    query(gameContract, inkVersion, "Option<u64>", "getPlayerSeed", [address]),
+  playerMtcImmutable: (address) =>
+    query(
+      gameContract,
+      inkVersion,
+      "Option<(Vec<mtc_Emo>, Vec<Option<(AccountId, mtc_Ghost)>>)>",
+      "getPlayerMtcImmutable",
+      [address]
     ),
-  leaderboard: async () =>
-    createType(
-      "Vec<(u16, AccountId)>",
-      (await queryContract(gameContract, "getLeaderboard")).toU8a()
-    ),
-  playerEp: async (address) =>
-    createType(
-      "Option<u16>",
-      (await queryContract(gameContract, "getPlayerEp", [address])).toU8a()
-    ),
-  playerSeed: async (address) =>
-    createType(
-      "Option<u64>",
-      (await queryContract(gameContract, "getPlayerSeed", [address])).toU8a()
-    ),
-  playerMtcImmutable: async (address) =>
-    createType(
-      "Option<(Vec<mtc_Emo>, Vec<(AccountId, mtc_Ghost)>)>",
-      (await queryContract(gameContract, "getPlayerMtcImmutable", [address])).toU8a()
-    ),
-  playerMtcMutable: async (address) =>
-    createType(
-      "Option<mtc_storage_PlayerMutable>",
-      (await queryContract(gameContract, "getPlayerMtcMutable", [address])).toU8a()
-    ),
+  playerMtcMutable: (address) =>
+    query(gameContract, inkVersion, "Option<mtc_storage_PlayerMutable>", "getPlayerMtcMutable", [
+      address,
+    ]),
 })
 
 const buildConnectionTx = (gameContract: ContractPromise): Connection["tx"] => ({

front/src/components/App/ConnectionProvider/tasks.ts
@@ -28,7 +28,7 @@ export interface Connection {
     playerSeed: (address: string) => Promise<Option<u64>>
     playerMtcImmutable: (
       address: string
-    ) => Promise<Option<ITuple<[Vec<mtc_Emo>, Vec<ITuple<[AccountId, mtc_Ghost]>>]>>>
+    ) => Promise<Option<ITuple<[Vec<mtc_Emo>, Vec<Option<ITuple<[AccountId, mtc_Ghost]>>>]>>>
     playerMtcMutable: (address: string) => Promise<Option<mtc_storage_PlayerMutable>>
   }
   tx: {

front/src/components/pages/Mtc/tasks.ts
@@ -40,10 +40,10 @@ export const start = (
 
     const ghosts = createType(
       "Vec<mtc_Ghost>",
-      _ghosts.map(([_account, ghost]) => ghost)
+      _ghosts.map((opt) => opt.unwrapOrDefault()[1])
     )
-    const ghostAddresses = _ghosts.map(([account, _ghost]) =>
-      connection.transformAddress(account.toString())
+    const ghostAddresses = _ghosts.map((opt) =>
+      connection.transformAddress(opt.unwrapOrDefault()[0].toString())
     )
 
     const mutable = _mutable.unwrap()

common/js/src/contract.ts
@@ -5,10 +5,22 @@ import { ContractPromise } from "@polkadot/api-contract"
 import { tx, buildErrorText, createType } from "./api"
 import type { KeyringPairOrAddressAndSigner } from "./utils"
 
-import gameAbi from "../../../contract/deploy/202109210_init/game.json"
+import gameAbiInk3 from "../../../contract/deploy/202109210_init/game.json"
+import gameAbiInk4 from "../../../contract/deploy/202305270_ink4/game.json"
 
-export const getGameContract = (api: ApiPromise, address: string) =>
-  new ContractPromise(api, gameAbi, address)
+export const getGameContract = (api: ApiPromise, address: string, inkVersion: number) => {
+  let abi
+
+  if (inkVersion === 3) {
+    abi = gameAbiInk3
+  } else if (inkVersion === 4) {
+    abi = gameAbiInk4
+  } else {
+    throw new Error(`undefined ink version: ${inkVersion}`)
+  }
+
+  return new ContractPromise(api, abi, address)
+}
 
 export const queryContract = async (
   contract: ContractPromise,

forbesus/OpenEmojiBattler

commit 33a2ba8a93ff4c68fb3cf6723bbd75f3489c5f96

front: Fix ink3 error
I unintentionally dropped the support of ink3 so fixed it.

The bug was introduced in: a07a8de7add5f541bd32d35f7fe7bdaeb5fe3be9

front/src/components/App/ConnectionProvider/Contract/tasks.ts
@@ -67,14 +67,27 @@ const buildConnectionQuery = (
   playerEp: (address) => query(gameContract, inkVersion, "Option<u16>", "getPlayerEp", [address]),
   playerSeed: (address) =>
     query(gameContract, inkVersion, "Option<u64>", "getPlayerSeed", [address]),
-  playerMtcImmutable: (address) =>
-    query(
-      gameContract,
-      inkVersion,
-      "Option<(Vec<mtc_Emo>, Vec<Option<(AccountId, mtc_Ghost)>>)>",
-      "getPlayerMtcImmutable",
-      [address]
-    ),
+  playerMtcImmutable: async (address) => {
+    const codec = (
+      await query(
+        gameContract,
+        inkVersion,
+        `Option<(Vec<mtc_Emo>, Vec<${inkVersion === 4 ? "Option<" : ""}(AccountId, mtc_Ghost)${
+          inkVersion === 4 ? ">" : ""
+        }>)>`,
+        "getPlayerMtcImmutable",
+        [address]
+      )
+    ).unwrap()
+
+    return [
+      codec[0],
+      codec[1].toArray().map((x: any) => {
+        const t = inkVersion === 4 ? x.unwrapOrDefault() : x
+        return [t[0], t[1]]
+      }),
+    ]
+  },
   playerMtcMutable: (address) =>
     query(gameContract, inkVersion, "Option<mtc_storage_PlayerMutable>", "getPlayerMtcMutable", [
       address,

front/src/components/App/ConnectionProvider/tasks.ts
@@ -26,9 +26,7 @@ export interface Connection {
     leaderboard: () => Promise<Vec<ITuple<[u16, AccountId]>>>
     playerEp: (address: string) => Promise<Option<u16>>
     playerSeed: (address: string) => Promise<Option<u64>>
-    playerMtcImmutable: (
-      address: string
-    ) => Promise<Option<ITuple<[Vec<mtc_Emo>, Vec<Option<ITuple<[AccountId, mtc_Ghost]>>>]>>>
+    playerMtcImmutable: (address: string) => Promise<[Vec<mtc_Emo>, Array<[AccountId, mtc_Ghost]>]>
     playerMtcMutable: (address: string) => Promise<Option<mtc_storage_PlayerMutable>>
   }
   tx: {

front/src/components/pages/Mtc/tasks.ts
@@ -36,15 +36,13 @@ export const start = (
       connection.query.playerMtcMutable(account.address),
     ])
 
-    const [pool, _ghosts] = immutable.unwrap()
+    const [pool, _ghosts] = immutable
 
     const ghosts = createType(
       "Vec<mtc_Ghost>",
-      _ghosts.map((opt) => opt.unwrapOrDefault()[1])
+      _ghosts.map(([_a, g]) => g)
     )
-    const ghostAddresses = _ghosts.map((opt) =>
-      connection.transformAddress(opt.unwrapOrDefault()[0].toString())
-    )
+    const ghostAddresses = _ghosts.map(([a, _g]) => connection.transformAddress(a.toString()))
 
     const mutable = _mutable.unwrap()
 